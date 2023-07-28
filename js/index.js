1ca0307fd0fcbe477abee72908f42b8c701e5baa
bacon <50970307+jakeupz@users.noreply.github.com>
2023-07-27 22:48:14 -0500

    Added images and a background image.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+const images = Array.from(document.querySelectorAll('.carousel__image'));
+let currentIndex = 0;
+
+setInterval(() => {
+    // remove active class from current image
+    images[currentIndex].classList.remove('active');
+
+    // go to next image
+    currentIndex = (currentIndex + 1) % images.length;
+
+    // add active class to next image
+    images[currentIndex].classList.add('active');
+}, 2000); // changes every 2 seconds
